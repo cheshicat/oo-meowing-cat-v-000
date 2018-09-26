3df8b3c6c964667035acfd10aa48d7e5ea1dcc2d
Allison Howard <alliemhow@gmail.com>
2018-09-26 11:22:56 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,10 +2,11 @@ class Cat
   attr_accessor :name
 
   def meow=(meowing_cat)
-    @this_cat_meowing = meowing_cat
+    @this_cat_meowing = "meow!"
   end
 
   def meow
     @this_cat_meowing
   end
+
 end
